0a008d0f2dc80c7f52dd6faa8fb4a35e601b0c15
Thanh131103 <87074256+Thanh131103@users.noreply.github.com>
2022-11-07 10:45:41 +0700

    first commit connect to back-end

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+
+const app = express()
+dotenv.config()
+const connect = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connect to mongoDB")
+    } catch (error) {
+        throw error;
+    }
+};
+
+app.listen(8800, () => {
+    console.log("Connect to backend.")
+})
