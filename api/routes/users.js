b544d9131a070bd2dced0b398786052031e4ea05
Thanh131103 <87074256+Thanh131103@users.noreply.github.com>
2022-11-11 10:25:19 +0700

    token for user

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,10 +2,13 @@ import express from "express";
 const router = express.Router();
 import { updateUser, deleteUser, getUser, getUsers } from "../controllers/user.js";
 import { createError } from "../untils/error.js";
+import { verifyToken } from "../untils/verifyToken.js";
 //update
 router.put("/:id", updateUser);
-
-//delete
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+        res.send("You are log in")
+    })
+    //delete
 router.delete("/:id", deleteUser);
 //Get
 router.get("/:id", getUser);
